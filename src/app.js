3bab0172a565593c455c097cbcd2d8e3b3bfc435
ankit0000 <ast.ankit1910@gmail.com>
2020-04-21 22:03:27 +0530

    test2

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      title: 'this is the App master chnage_6',
+      title: 'this is the App master chnage_67',
       test_3: 'test_3',
       title1: 'this is the App master title1',
       t: 't',
